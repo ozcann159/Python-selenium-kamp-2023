11aa8a877fb594676a2b79a962063db03bf1fa73
Elif Özcan <ozcann159@gmail.com>
2023-03-11 00:36:50 +0300

    Create Odev1.py

---
+++ 1. Tabanca/Odev1.py
@@ -0,0 +1,48 @@
+# Ödev 1.Gun Cevaplar
+#Pyhton'da ki Veri Tipleri:
+#Soru 1
+#String (metin) veri tipleri → str : Metinsel ifadelerin yer aldığı bir veri tipidir. "Hello World"
+#Sayısal yani numerik veri tipleri → int=20 Integer, tamsayı şeklinde veri tipidir.
+#Float veri tipi : ondalıklı sayı tutar. Ornegin;  15.5
+#compex:Karışık olarak verininb bulunduğu veri tipidir.
+#list=["pazartesi","salı","carsamba",2,9,0] #List, içerisinde eleman barındırabilen listeyi ifade eder.
+
+#Liste elamanları güncellenebilir.
+tuple1=1,"iki",3 #Tuple, liste gibi elemanlar bulunduran veri tipidir.
+tuple2=(5,6,"yedi") #Tuple'da eleman güncellemesi yapamayız.
+
+#range=range(10) #Range, aralık,uzunluk(mesafe) belirten veri tipidir.
+#dict={"kocaeli":41 , "istanbul":34} #Dictionary, liste benzeri her metinin bir değeri olması gereken veri tipidir.
+##Dictionary'de güncelleme yapapbliriz. key:value
+#set={"Germany","France","Netherlands"} #Set, liste benzeri veri tipidir.
+#Set'de elemanlar indexlenemez, sıralanamaz, birden fazla aynı eleman eklenemez.
+
+#bool=True #Boolean, sadece 2 çeşit değer alan veri tipidir(True ya da False).
+
+
+#Soru 2:Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünü verileri veri tipleriyle birlikte örneklendiriniz.
+#String ifadeler : Kurs isimleri, eğitmen isim, açıklamalar
+#Float: Kursu tamamlama yüzdesi kısımı 
+#Boolean: Ödevlerin tamamlanmış olup olmaması
+#integer: yorum satırları
+
+
+# Kodlama.io sitesinde şart blokları kullanıldığı düşündüğünüz kısımları örneklendiriniz ve Python dilnde koda döküyiz
+
+#if ogrenciTamamlamaYuzdesi == 100:
+#    print("Kurs Başarıyla Tamamlandı")
+
+#else:
+#    print("Kurs Bitmedi")
+
+#if butunKurs == "(Kurs tamamlandı)
+#     print("Birinci Odev Tamamlandı")
+
+#else: 
+#    print("Birinci Odev tamamlanmadı Lutfen Odevı Yapınız")
+
+#if sifre =="123"
+#   print("Giris Basarılı")
+
+#else:
+#    print("Yanlis Sifre Girdiniz")
